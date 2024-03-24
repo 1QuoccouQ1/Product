c43b19c8435093525305e5e3fc3736969b90e19b
1QuoccouQ1 <bqgaming2004@gmail.com>
2024-03-24 18:50:46 +0700

    good

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function chuyenDenLink() {
+    window.location.href = "index.html";
+  }
+var With = document.querySelector(".header-list-li");
+
+  
+var chieuDai = With.clientWidth;
+var setWith = chieuDai + "px"
+
+With.style.setProperty("--chieudai", setWith);
+
+
+
+function toggleLoginMobile() {
+  var loginMobile = document.getElementById("loginMobile");
+  loginMobile.style.display = (loginMobile.style.display === "none") ? "block" : "none";
+}
+
+var loginMobileTopIcon = document.querySelector(".login-mobile-top-icon");
+loginMobileTopIcon.addEventListener("click", function(event) {
+  event.stopPropagation();
+  toggleLoginMobile();
+});
+
+
+
+
